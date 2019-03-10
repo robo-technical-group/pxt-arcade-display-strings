9ff5a3221b62112fad3b3d748e1fd942692a6a66
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2019-03-09 23:32:34 -0500

    Major refactor. Add tests for centered text.

--- displayStrings.ts
+++ displayStrings.ts
@@ -1,17 +1,121 @@
 enum FontName {
-    //% block="Font5"
-    Font5 = 0,
-    //% block="Font8"
-    Font8
+    Font5,
+    Font8,
+    Default
 }   // enum Font
+
+/**
+ * Immutable class that represents a font.
+ */
+//% blockNamespace=drawStrings
+class FontInfo {
+    private _fontName: FontName;
+    private _size: number;
+    private _font: image.Font;
+
+    /**
+     * Default constructor.
+     * @param {FontName} font - Font to use.
+     * @param {number} size - Size of font.
+     */
+    constructor(font: FontName, size: number) {
+        this._fontName = font;
+        this._size = size;
+        let f: image.Font;
+        switch (font) {
+            case FontName.Font5:
+                f = image.font5;
+                break;
+
+            case FontName.Font8:
+            case FontName.Default:
+            default:
+                f = image.font8;
+                break;
+        }   // switch (font)
+
+        switch (size) {
+            case 1:
+                this._font = f;
+                break;
+
+            case 2:
+                this._font = image.doubledFont(f);
+                break;
+
+            default:
+                this._font = image.scaledFont(f, size);
+                break;
+        }   // switch (size)
+    }   // constructor()
+
+    /**
+     * Returns the font described by the object.
+     * @return {image.Font} The font for the object.
+    */
+    public get font(): image.Font {
+        return this._font;
+    }   // get font()
+
+    /**
+     * Returns the height of the font.
+     * @return {number} The font's height.
+     */
+    public get height(): number {
+        return this._font.charHeight;
+    }   // height()
+
+    /**
+     * Returns the font name of the object.
+     * @return {FontName} The font name for the object.
+     */
+    public get fontName(): FontName {
+        return this._fontName;
+    }   // get fontName()
+
+    /**
+     * Returns the size of the font.
+     * @return {number} The font size.
+     */
+    public get size(): number {
+        return this._size;
+    }   // get size()
+
+    /**
+     * Returns the width of a string in the specified font.
+     * @param {string} text - The text to measure.
+     * @return {number} - The width of the text in pixels.
+     */
+    public width(text: string): number {
+        return (this._font.charWidth + 1) * text.length;
+    }   // get width()
+}   // class FontInfo
+
+
 
 /**
  * Extension for writing text to an image.
  */
 //% weight=0 color=#b8860b icon="\uf040" block="Draw Text"
 //% advanced=true
-namespace displayStrings {
-    const defaultFont: image.Font = image.font8;
+namespace drawStrings {
+    const DEFAULT_FONT: FontName = FontName.Font8;
+    const DEFAULT_SIZE: number = 1;
+
+
+    /**
+     * Create a font info object.
+     */
+    //% blockId=drawstring_createFont
+    //% block="font %font || with size %size"
+    //% font.defl=FontName.Font8 size.defl=1
+    export function createFontInfo(font: FontName,
+        size: number = 0): FontInfo {
+        if (size < 1) {
+            size = DEFAULT_SIZE;
+        }   // if (!size)
+        return new FontInfo(font, size);
+    }   // createFont()
 
     /**
      * Width of a string in pixels for the given font.
@@ -19,11 +123,11 @@ namespace displayStrings {
     //% blockId=drawstring_width
     //% block="width of %text in %font"
     //% text.defl="Hello!"
-    export function width(text: string, font: image.Font): number {
-        if (!font) {
-            font = defaultFont;
-        }   // if (!font)
-        return text.length * font.charWidth;
+    export function width(text: string, fi: FontInfo): number {
+        if (!fi) {
+            fi = createFontInfo(DEFAULT_FONT);
+        }   // if (!fi)
+        return fi.width(text);
     }   // width()
 
     /**
@@ -31,8 +135,11 @@ namespace displayStrings {
      */
     //% blockId=drawstring_height
     //% block="height of font %font"
-    export function height(font: image.Font): number {
-        return font.charHeight;
+    export function height(fi: FontInfo): number {
+        if (!fi) {
+            fi = createFontInfo(DEFAULT_FONT);
+        }   // if (!fi)
+        return fi.height;
     }   // height()
 
     /**
@@ -42,12 +149,12 @@ namespace displayStrings {
     //% block="write %text on image %img=screen_image_picker at x %x y %y with color %color=colorindexpicker in font %font"
     //% text.defl="Hello!" x.defl=0 y.defl=0 color.defl=1 color.min=0 color.max=15
     export function write(text: string, img: Image,
-        x: number, y: number, color: color = 1, font: image.Font)
+        x: number, y: number, color: color, fi: FontInfo)
         : void {
-        if (!font) {
-            font = defaultFont;
-        }   // if (!font)
-        img.print(text, x, y, color, font);
+        if (!fi) {
+            fi = createFontInfo(DEFAULT_FONT);
+        }   // if (!fi)
+        img.print(text, x, y, color, fi.font);
     }   // write()
 
     /**
@@ -57,61 +164,10 @@ namespace displayStrings {
     //% block="write %text centered on image %img=screen_image_picker at y %y with color %color=colorindexpicker in font %font"
     //% text.defl="Hello!" x.defl=0 y.defl=0 color.defl=1
     export function writeCenter(text: string, img: Image,
-        y: number, color: color = 1, font: image.Font): void {
-        if (!font) {
-            font = defaultFont;
-        }   // if (!font)
-        img.printCenter(text, y, color, font);
+        y: number, color: color, fi: FontInfo): void {
+        if (!fi) {
+            fi = createFontInfo(DEFAULT_FONT);
+        }   // if (!fi)
+        img.printCenter(text, y, color, fi.font);
     }   // writeCenter()
-
-    /**
-     * Create a font object.
-     */
-    //% blockId=drawstring_font
-    //% block="font %font"
-    //% font.defl=Font.Font8
-    export function font(font: FontName): image.Font {
-        switch (font) {
-            case FontName.Font5:
-                return image.font5;
-
-            case FontName.Font8:
-            default:
-                return image.font8;
-        }   // switch ( font )
-    }   // font()
-
-    /**
-     * Create a font object with size doubled.
-     */
-    //% blockId=drawstring_font_double
-    //% block="font %font double-sized"
-    //% font.defl=Font.Font8
-    export function fontDouble(font: FontName): image.Font {
-        switch (font) {
-            case FontName.Font5:
-                return image.doubledFont(image.font5);
-
-            case FontName.Font8:
-            default:
-                return image.doubledFont(image.font8);
-        }   // switch ( font )
-    }   // fontDouble()
-
-    /**
-     * Create a font object scaled by a given multiplier.
-     */
-    //% blockId=drawstring_font_scaled
-    //% block="font %font scaled %scale times"
-    //% font.defl=Font.Font8 scale.defl=2
-    export function fontScaled(font: FontName, scale: number): image.Font {
-        switch (font) {
-            case FontName.Font5:
-                return image.scaledFont(image.font5, scale);
-
-            case FontName.Font8:
-            default:
-                return image.scaledFont(image.font8, scale);
-        }   // fontScaled()
-    }   // fontScaled()
-}   // namespace displayStrings
+}   // namespace drawStrings
--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
+let background: Image = null;
+let pauseTime: number = 2000;
 let strings: string[] = [
     "Font5",
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -7,9 +9,7 @@ let strings: string[] = [
     "Font8",
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     "abcdefghijklmnopqrstuvwxyz",
-    "0123456789",
-];
-
+    "0123456789"];
 let fonts: FontName[] = [
     FontName.Font5,
     FontName.Font5,
@@ -19,37 +19,28 @@ let fonts: FontName[] = [
     FontName.Font8,
     FontName.Font8,
     FontName.Font8,
-    FontName.Font8,
-];
+    FontName.Font8];
 
-let pauseTime: number = 2000;
+for (let size: number = 1; size <= 3; size++) {
+    background = image.create(scene.screenWidth(), scene.screenHeight());
+    let y: number = 0;
+    for (let index = 0; index <= strings.length - 1; index++) {
+        let f: FontInfo = drawStrings.createFontInfo(fonts[index], size);
+        drawStrings.write(strings[index], background, 0, y, index + 1, f);
+        y += drawStrings.height(f) + 1;
+    }   // for (index)
+    scene.setBackgroundImage(background);
+    loops.pause(pauseTime);
+}   // for ( size )
 
-let background: Image = image.create(scene.screenWidth(), scene.screenHeight());
-let y: number = 0;
-for (let index: number = 0; index < strings.length; index++) {
-    let f = displayStrings.font(fonts[index]);
-    displayStrings.write(strings[index], background, 0, y, index + 1, f);
-    y += f.charHeight + 1;
-}   // for ( index )
-scene.setBackgroundImage(background);
-loops.pause(pauseTime);
-
-background = image.create(scene.screenWidth(), scene.screenHeight());
-y = 0;
-for (let index: number = 0; index < strings.length; index++) {
-    let f = displayStrings.fontDouble(fonts[index]);
-    displayStrings.write(strings[index], background, 0, y, index + 1, f);
-    y += f.charHeight + 1;
-}   // for ( index )
-scene.setBackgroundImage(background);
-loops.pause(pauseTime);
-
-background = image.create(scene.screenWidth(), scene.screenHeight());
-y = 0;
-for (let index: number = 0; index < strings.length; index++) {
-    let f = displayStrings.fontScaled(fonts[index], 3);
-    displayStrings.write(strings[index], background, 0, y, index + 1, f);
-    y += f.charHeight + 1;
-}   // for ( index )
-scene.setBackgroundImage(background);
-
+for (let size: number = 1; size <= 3; size++) {
+    background = image.create(scene.screenWidth(), scene.screenHeight());
+    let y: number = 0;
+    for (let index = 0; index <= strings.length - 1; index++) {
+        let f: FontInfo = drawStrings.createFontInfo(fonts[index], size);
+        drawStrings.writeCenter(strings[index], background, y, index + 1, f);
+        y += drawStrings.height(f) + 1;
+    }   // for (index)
+    scene.setBackgroundImage(background);
+    loops.pause(pauseTime);
+}   // for ( size )
